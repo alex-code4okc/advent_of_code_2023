59c3ff914580eb41bb22e2983e61776a87f22eea
Alex Ayon <jayonloza@gmail.com>
2023-12-17 10:15:29 -0700

    day 8 part 1 complete

--- Day8/day8.py
+++ Day8/day8.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 def read_file(filename: str) -> list[str]:
@@ -8,13 +9,62 @@ def read_file(filename: str) -> list[str]:
         file_contents = FILE.readlines()
     return file_contents
 
+direction_mapping = {
+    'R': 1,
+    'L': 0,
+}
+
+def setup():
+    lines = read_file('input.txt')
+    directions = lines[0].strip()
+    directions_map = {}
+    for line in lines[2:]:
+        matches = re.findall(r'\w+', line)
+        directions_map[matches[0]] = (matches[1],matches[2])
+    return (directions, directions_map)
+
 def part1():
-    pass
+    directions, mapping = setup()
+    current_node = mapping['AAA']
+    # loop
+    i = 0
+    n = len(directions)
+    steps = 0
+    while True:
+        direction = directions[i]
+        index = direction_mapping[direction]
+        next_node_key = current_node[index]
+        i = (i+1)%n
+        steps += 1
+        current_node = mapping[next_node_key]
+        if next_node_key == 'ZZZ':
+            break
+    return steps
 
 def part2():
-    pass
+    directions, mapping = setup()
+    keys_A = [k for k in mapping.keys() if k[2] == 'A']
+    initial_paths_count = len(keys_A)
+    current_nodes = [mapping[k] for k in keys_A]
+    # loop
+    i = 0
+    n = len(directions)
+    steps = 0
+    while True:
+        direction = directions[i]
+        index = direction_mapping[direction]
+        next_node_keys = [cn[index] for cn in current_nodes]
+        i = (i+1)%n
+        steps += 1
+        current_nodes = [mapping[k] for k in next_node_keys]
+        all_Z = [k[2] for k in next_node_keys]
+        if all_Z.count('Z') == initial_paths_count:
+            break
+    return steps
 
 def results():
-    answer1 = part1()
+    #answer1 = part1()
     answer2 = part2()
     pass
+
+results()
